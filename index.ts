c68327592dd90b6b4a058aabb4bcea45322538a0
Ben Blackburne <bpb@lysozyme.net>
2016-10-12 11:11:38 +0100

    Typescript version

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-const ffi = require('ffi');
-const url = require('url');
+import * as ffi from 'ffi';
+import * as url from 'url'
 
 const aliasResolver = ffi.Library(__dirname + '/dist/libAliasResolver', {
   'resolveAliasToBuffer' : [ 'bool' , [ 'CString', 'char *', 'ulong', 'ulong *' ] ]
 });
 
 
-function resolveAliasWithBuffer(path, initialBufferSize) {
+function resolveAliasWithBuffer(path: string, initialBufferSize: number): string | null {
   const buffer = Buffer.alloc(initialBufferSize);
   const sizeBuffer = Buffer.alloc(4);
 
@@ -15,9 +15,9 @@ function resolveAliasWithBuffer(path, initialBufferSize) {
     return null;
   }
 
-  const neededBuffer = sizeBuffer.readUInt32LE();
+  const neededBuffer = sizeBuffer.readUInt32LE(0);
   if (neededBuffer + 1 > initialBufferSize) {
-    return resolveAlias(path, neededBuffer + 1);
+    return resolveAliasWithBuffer(path, neededBuffer + 1);
   }
 
   let str = buffer.toString('utf8');
@@ -25,15 +25,15 @@ function resolveAliasWithBuffer(path, initialBufferSize) {
   return str;
 }
 
-function resolveAlias(path) {
-  const fileURIString = resolveAliasWithBuffer(path, 512);
+function fmap<T,U>(val: T | null, f: (v:T) => U): U | null {
+  return val ? f(val) : null;
+}
 
-  if (!fileURIString) {
-    return null;
-  }
-
-  const fileURL = url.parse(fileURIString);
-  return decodeURI(fileURL.path);
+function resolveAlias(path: string): string | null {
+  return fmap(resolveAliasWithBuffer(path, 512), fileURIString => {
+    const fileURL = url.parse(fileURIString);
+    return fileURL.path ? decodeURI(path) : null; 
+  });
 }
 
 module.exports = resolveAlias;
